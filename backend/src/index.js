f98b3ba3bdd26467d18d01eeae7194d655ab3499
Pourya Zardosht <pooriyazardosht.812@gmail.com>
2025-01-21 00:43:22 +0330

    feat: project ready to deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
@@ -10,7 +12,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const port = process.env.PORT;
-
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -22,6 +24,14 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(port, () => {
   console.log("server is running on port " + port);
